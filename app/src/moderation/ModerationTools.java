5f02f9e59da13a5cc81a4c75cdf0b12cd261e6bf
Frank Zhou <u8256861@anu.edu.au>
2026-05-08 17:29:34 +1000

    Task 4

--- app/src/moderation/ModerationTools.java
+++ app/src/moderation/ModerationTools.java
@@ -1,9 +1,12 @@
 package moderation;
+import dao.MostReportedComparator;
 import dao.PostDAO;
 import dao.UserDAO;
 import dao.MessageComparator;
 import dao.model.Message;
 import sorteddata.sortedarraylist.SortedArrayList;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import dao.model.User;
 import java.util.Iterator;
@@ -74,7 +77,7 @@ public class ModerationTools {
         return true;
 	}
 	
-	public static Iterator<Message> getReportedMessages(String strategy, int amount) {
+	public static Iterator<Message> getReportedMessages(String strategy, int amount) throws IOException {
         List<Report> reportList = AllReports.allReports;
         List<Message> messageList = new ArrayList<>();
         Iterator<Message> allMessages = PostDAO.getInstance().getAllMessages();
@@ -91,6 +94,7 @@ public class ModerationTools {
             if (message != null) messageList.add(message);
         }
 
+        IOException IOException = new IOException();
         switch (strategy) {
             case "OLDEST" -> {
                 SortedArrayList<Message> sorted = new SortedArrayList<>(MessageComparator.getInstance());
@@ -100,9 +104,13 @@ public class ModerationTools {
                 return sorted.getAll();
             }
             case "MOST" -> {
-                SortedArrayList<Message> sorted = new SortedArrayList<>(MessageComparator.getInstance());
-            };
-            default -> ;
+                SortedArrayList<Message> sorted = new SortedArrayList<>(MostReportedComparator.getInstance());
+                for (Message message : messageList) {
+                    sorted.insert(message);
+                }
+                return sorted.getAll();
+            }
+            default -> throw IOException;
         }
     }
 }
